d9584ed7408c528fbff702c9784b2779c11b0531
HuzaifaBanegar <banegarhuzzi1010@gmail.com>
2022-03-31 20:09:35 +0530

    updated

--- src/index.js
+++ src/index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const bestSellerController = require("./controllers/bestSellerController");
+
+//Updated Validation for these paths
 const login= require("../src/controllers/logincontroller");
 const register= require("../src/controllers/registerController")
-// const {register, login}= require("../src/controllers/authController")
+// const {register, login}= require("../src/controllers/authController") ---->this register and login doesnt contain validation
 
 
 

67b3ba92f476652a87719c239ffa872a38430794
Gaurav Arsod <gaarsod14@gmail.com>
2022-04-03 11:01:15 +0530

    push

--- src/models/allProductsModel.js
+++ src/models/allProductsModel.js
@@ -13,7 +13,7 @@ const productSchema = new mongoose.Schema(
         Qty: {type: Number, required: true}
     },
     {
-        versionKey: true,
+        versionKey: false,
         timestamps: true
     }
 );
--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
+require("dotenv").config();
 const app = require("./index");
 const connect = require("./configs/db");
+const port = process.env.PORT || 5000;
 
-app.listen(5000, async() => {
+app.listen(port, async() => {
     try{
         await connect();
-        console.log("Listening to port 5000 of Bella_Vita_Organic_backend");
+        console.log(`Listening to port ${port} of Bella_Vita_Organic_backend`);
     }
     catch(err) {
         console.error(err);

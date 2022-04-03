ecf26a7508ff1c399ab66f936e8a559a031fd4a9
HuzaifaBanegar <banegarhuzzi1010@gmail.com>
2022-04-03 09:35:27 +0530

    Update userModel.js

--- src/models/userModel.js
+++ src/models/userModel.js
@@ -6,7 +6,8 @@ const userSchema= new mongoose.Schema({
     firstName:{type:String, required:true},
     lastName:{type:String, required:true},
     email:{type:String, required:true},
-    password:{type:String, required:true}
+    password:{type:String, required:true},
+    role:{type:String, required:false}
 },{
     timestamp:true,
     versionKey:false

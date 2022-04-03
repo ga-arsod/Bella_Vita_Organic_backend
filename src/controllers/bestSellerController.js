ef058e045f7f211753273d38604dbd8a789d314b
Gaurav Arsod <gaarsod14@gmail.com>
2022-04-03 12:34:16 +0530

    Update bestSellerController.js

--- src/controllers/bestSellerController.js
+++ src/controllers/bestSellerController.js
@@ -23,34 +23,43 @@ router.post("", async(req, res) => {
 
 
 router.get("", async(req, res) => {
+    // try{
+    //     const page = +req.query.page || 1;
+    //     const pageSize = +req.query.size || 21;
+
+    //     client.get(`bestsellers`, async function(err, fetchedProducts) {
+    //         if(fetchedProducts) {
+    //             const products = JSON.parse(fetchedProducts);
+
+    //             return res.status(201).send(products);
+    //         }
+    //         else {
+    //             try{
+    //                 const offset = (page - 1) * pageSize;
+
+    //                 const products = await Product.find({}).lean().exec();
+
+    //                 client.set(`bessellers`, JSON.stringify(products));
+
+    //                 return res.status(200).send(products);
+    //             }
+    //             catch(err) {
+    //                 return res.status(500).send({error: err.message});
+    //             }
+    //         }
+    //     })
+    // }
+    // catch(err) {
+    //     return res.status(401).send({error: err.message});
+    // }
+
     try{
-        const page = +req.query.page || 1;
-        const pageSize = +req.query.size || 21;
+        return res.status(300).send("Bessdfsdfsdf");
 
-        client.get(`bestsellers`, async function(err, fetchedProducts) {
-            if(fetchedProducts) {
-                const products = JSON.parse(fetchedProducts);
-
-                return res.status(201).send(products);
-            }
-            else {
-                try{
-                    const offset = (page - 1) * pageSize;
-
-                    const products = await Product.find({}).lean().exec();
-
-                    client.set(`bessellers`, JSON.stringify(products));
-
-                    return res.status(200).send(products);
-                }
-                catch(err) {
-                    return res.status(500).send({error: err.message});
-                }
-            }
-        })
     }
     catch(err) {
-        return res.status(401).send({error: err.message});
+        return res.status(300).send(err);
+
     }
 });
 
